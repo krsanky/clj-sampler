a6d4937c71eac0e58edb5cdc47bf6203ceb18d4d
Paul Wisehart <paul@1ud2.com>
2011-01-02 15:32:56 -0500

    make core haave simple swing window pop up with a no-op button

--- src/clj_sampler/core.clj
+++ src/clj_sampler/core.clj
@@ -1,1 +1,19 @@
 (ns clj-sampler.core)
+
+
+;;(def frame (JFrame. "Hello Frame"))
+;;(.setSize frame 200 200)
+;;(.setVisible frame true)
+
+
+(import '(javax.swing JFrame JPanel JButton))
+
+(def button (JButton. "Click Me!"))
+
+(def panel (doto (JPanel.)
+             (.add button)))
+
+(def frame (doto (JFrame. "Hello Frame")
+             (.setSize 200 200)
+             (.setContentPane panel)
+             (.setVisible true)))
