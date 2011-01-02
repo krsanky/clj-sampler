3f07ad6587801beebb6264a6975714dc38191eaa
Paul Wisehart <paul@1ud2.com>
2011-01-02 15:32:32 -0500

    add swank-clojure dev dep

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clj-sampler "0.0.1-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]])
 
