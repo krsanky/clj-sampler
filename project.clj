87c9a4a01f2cb8ae5f9eee784b9fbadaf276c560
Paul Wisehart <paul@1ud2.com>
2011-01-02 12:16:54 -0500

    add skel lein project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-sampler "0.0.1-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
+
---
+++ src/clj_sampler/core.clj
@@ -0,0 +1,1 @@
+(ns clj-sampler.core)
