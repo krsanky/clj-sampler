93394aca818ec0eb15bb383a33fafaf5ae368db2
Paul Wisehart <paul@1ud2.com>
2011-01-15 14:41:10 -0500

    using jgoodies forms, and adding debug panel

--- project.clj
+++ project.clj
@@ -2,7 +2,10 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
                  [org.clojure.contrib/standalone "1.3.0-alpha4"]
-                 [org.clojars.nakkaya/forms "1.2.1"]]
+                 ;;[org.clojars.nakkaya/forms "1.2.1"]
+                 [jgoodies/common "1.1.1"]
+                 [jgoodies/forms "1.4.0"]]
+
   :dev-dependencies [[swank-clojure "1.2.0"]]
   :main clj-sampler.main)
 
@@ -13,3 +16,8 @@
 ;;[org.clojure.contrib/standalone "1.3.0-alpha2"]
 ;;http://build.clojure.org/releases/org/clojure/clojure/1.3.0-alpha4/
 
+
+;; <repository>
+;;   <id>clojars.org</id>
+;;   <url>http://clojars.org/repo</url>
+;; </repository>
--- src/clj_sampler/gui.clj
+++ src/clj_sampler/gui.clj
@@ -5,7 +5,8 @@
    com.jgoodies.forms.layout.CellConstraints
    com.jgoodies.forms.layout.FormLayout
    com.jgoodies.forms.factories.ButtonBarFactory
-   com.jgoodies.forms.builder.PanelBuilder)
+   com.jgoodies.forms.builder.PanelBuilder
+   com.jgoodies.forms.debug.FormDebugPanel)
   (:use
    [clojure.contrib.swing-utils
     :only (do-swing do-swing* add-action-listener)]))
@@ -16,22 +17,26 @@
   []
   (let [items     (take 5 (iterate inc 0))
         frame     (JFrame. "Example GUI")
-        layout    (FormLayout. "fill:default:grow" "pref,3dlu,pref")
+        layout    (FormLayout. "fill:default:grow" ;; cols
+                               "pref,3dlu,pref")   ;; rows
         cc        (CellConstraints.)
         done?     (atom false)
-        done      (JButton. "Done")
+        quit      (JButton. "quit")
         dummy     (JButton. "dummy")
-        bbar      (ButtonBarFactory/buildCenteredBar (into-array [done dummy]))
-        panel     (-> (PanelBuilder. layout)
+        bbar      (ButtonBarFactory/buildCenteredBar (into-array [quit dummy]))
+
+        panel     (-> (PanelBuilder. layout (FormDebugPanel.))  ;;rm the panel arg to use default "non-debug"
                       (doto (.setDefaultDialogBorder)
                         ;;(.add pbar (.xy cc 1 1))
                         (.add bbar (.xy cc 1 3)))
+
                       .getPanel)]
+
 
     (.setEnabled dummy false)
 
-    ;; Wire up the done button.
-    (add-action-listener done
+    ;; Wire up the quit button.
+    (add-action-listener quit
                          (fn [_]
                            (do-swing
                             (doto frame
